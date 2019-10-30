c38ee2d24692a48a2767aaa989545566f2059141
Heather Faerber <heatherdfaerber@gmail.com>
2019-10-30 16:13:43 -0600

    Add form html and js psuedocode

--- src/index.js
+++ src/index.js
@@ -8,7 +8,34 @@ import $ from 'jquery';
 import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
 import './images/concierge.jpg'
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+// psuedocoding
+// event listener on customer login button that expands customer login form
+// event listener on team member login button that expands team member login form
+
+// event listener on customer submit button that checks if password === correct
+// and if username is valid with id
+// IF NOT  error message try again
+// IF CORRECT display customer page
+// ^^same for manager but with manager stuff
+
+// EVENT LISTENERS
+
+
+// HANDLERS
+
+// do i need to worry about upper vs lowercase?
+function validateLogin() {
+  if ( $('#team-member-login').val() === 'manager' &&
+    $('#team-member-password').val() === 'overlook2019') {
+      return 'manager';
+  } else if ($('#customer-login').val().includes('customer') &&
+    $('#customer-password').val() === 'overlook2019') {
+      return 'customer';
+  } else {
+    return 'error'
+  }
+}
