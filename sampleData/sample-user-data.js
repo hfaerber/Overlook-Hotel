50647251fed17721f907c38f2fc6a23fb8053303
Heather Faerber <heatherdfaerber@gmail.com>
2019-10-31 21:48:48 -0600

    Create sample data sets and build tests and methods for tapechart class

---
+++ sampleData/sample-user-data.js
@@ -0,0 +1,84 @@
+const userData = [
+  {
+  id: 1,
+  name: "Leatha Ullrich"
+  },
+  {
+  id: 2,
+  name: "Rocio Schuster"
+  },
+  {
+  id: 3,
+  name: "Kelvin Schiller"
+  },
+  {
+  id: 4,
+  name: "Kennedi Emard"
+  },
+  {
+  id: 5,
+  name: "Rhiannon Little"
+  },
+  {
+  id: 6,
+  name: "Fleta Schuppe"
+  },
+  {
+  id: 7,
+  name: "Dell Rath"
+  },
+  {
+  id: 8,
+  name: "Era Hand"
+  },
+  {
+  id: 9,
+  name: "Faustino Quitzon"
+  },
+  {
+  id: 10,
+  name: "Tony Armstrong"
+  },
+  {
+  id: 11,
+  name: "Melissa Sauer"
+  },
+  {
+  id: 12,
+  name: "Kari Keeling"
+  },
+  {
+  id: 13,
+  name: "Christina Kulas"
+  },
+  {
+  id: 14,
+  name: "Dallas Schultz"
+  },
+  {
+  id: 15,
+  name: "Maria Lakin"
+  },
+  {
+  id: 16,
+  name: "Garry Mills"
+  },
+  {
+  id: 17,
+  name: "Trudie Grimes"
+  },
+  {
+  id: 18,
+  name: "Triston Leffler"
+  },
+  {
+  id: 19,
+  name: "Leatha Hettinger"
+  },
+  {
+  id: 20,
+  name: "Keon Kirlin"
+  }
+]
+
+export default userData;
